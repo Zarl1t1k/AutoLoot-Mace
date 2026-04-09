abd188bf27fbbe6abd58b52303706a28d9a404e8
Zarl1t1k <zarl1t1k@gmail.com>
2026-04-09 11:11:59 +0300

    Update HandledScreenMixin.java

--- src/main/java/com/autolootmace/mixin/HandledScreenMixin.java
+++ src/main/java/com/autolootmace/mixin/HandledScreenMixin.java
@@ -3,19 +3,19 @@ package com.autolootmace.mixin;
 import com.autolootmace.config.ModConfig;
 import com.autolootmace.util.AutoClicker;
 import com.autolootmace.util.LootDetector;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(HandledScreen.class)
+@Mixin(AbstractContainerScreen.class)
 public class HandledScreenMixin {
 
     @Inject(method = "render", at = @At("TAIL"))
-    private void onRender(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        HandledScreen<?> self = (HandledScreen<?>) (Object) this;
+    private void onRender(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick, CallbackInfo ci) {
+        AbstractContainerScreen<?> self = (AbstractContainerScreen<?>) (Object) this;
         ModConfig config = ModConfig.getInstance();
 
         if (!config.modEnabled) return;
